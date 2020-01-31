50a6e0e5fa77fdea886c0a2ad6c7adbc045709ea
Karine Samyn <Karine.Samyn@hogent.be>
2020-01-31 14:03:38 +0100

    Add POST, PUT, DELETE Recipe

--- RecipeApi/Controllers/RecipesController.cs
+++ RecipeApi/Controllers/RecipesController.cs
@@ -31,5 +31,44 @@ namespace RecipeApi.Controllers
             if (recipe == null) return NotFound();
             return recipe;
         }
+
+        // POST: api/Recipes
+        [HttpPost]
+        public ActionResult<Recipe> PostRecipe(Recipe recipe)
+        {
+            _recipeRepository.Add(recipe);
+            _recipeRepository.SaveChanges();
+
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.Id }, recipe);
+        }
+
+        // PUT: api/Recipes/5
+        [HttpPut("{id}")]
+        public IActionResult PutRecipe(int id, Recipe recipe)
+        {
+            if (id != recipe.Id)
+            {
+                return BadRequest();
+            }
+            _recipeRepository.Update(recipe);
+            _recipeRepository.SaveChanges();
+            return NoContent();
+        }
+
+
+        // DELETE: api/Recipes/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRecipe(int id)
+        {
+            Recipe recipe = _recipeRepository.GetBy(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            _recipeRepository.Delete(recipe);
+            _recipeRepository.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
