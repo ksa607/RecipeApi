4a9f120f3e9aabd8b27cd0bd1495ca4d9602e91c
Karine Samyn <Karine.Samyn@hogent.be>
2020-03-05 23:01:48 +0100

    Add example of n:m for CustomerController

---
+++ RecipeApi/DTOs/CustomerDTO.cs
@@ -0,0 +1,26 @@
+﻿using RecipeApi.Models;
+using System.Collections.Generic;
+
+namespace RecipeApi.DTOs
+{
+    public class CustomerDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<Recipe> Recipes { get; set; }
+
+        public CustomerDTO() { }
+
+        public CustomerDTO(Customer customer) : this()
+        {
+            FirstName = customer.FirstName;
+            LastName = customer.LastName;
+            Email = customer.Email;
+            Recipes = customer.FavoriteRecipes;
+        }
+    }
+}
